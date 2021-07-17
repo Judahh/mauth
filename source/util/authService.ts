c2fa6abb9a0f4443c4b6084f50841f1195321568
Judah <judahholanda7@gmail.com>
2021-07-17 17:28:13 -0300

    fix types

--- source/util/authService.ts
+++ source/util/authService.ts
@@ -1,8 +1,10 @@
+import Identification from './identification';
+
 /* eslint-disable no-unused-vars */
 export default interface AuthService {
   verify(
-    identification: unknown,
-    identifications: unknown,
-    headers: unknown
+    identification: Identification,
+    identifications: Identification[],
+    headers?: Headers
   ): Promise<void>;
 }
