f2af4673769ee89db479e9a8c5ff6f8774ecca14
Judahh <judahholanda@gmail.com>
2021-08-18 10:06:51 -0300

    update packages and remove comments

--- source/permission.ts
+++ source/permission.ts
@@ -15,7 +15,7 @@ export default class Permission {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  protected constructor() {}
+  protected constructor() { }
 
   static getInstance(): Permission {
     if (!this._instance) {
